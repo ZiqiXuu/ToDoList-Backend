35325c10a23f6084481a3f29d6dc2ec1b16cf17c
ZiqiXuu <zq952133@dal.ca>
2023-08-10 00:15:16 -0400

    version-3.0

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,11 @@ app.use(cors());
 dotenv.config()
 
 // register routes
-app.get("/todos", TodoController.getTodo)
-app.get("/todos/search", TodoController.getByName)
-app.post("/todos/insert", TodoController.create)
-app.delete("/todos/delete", TodoController.delete)
-app.put("/todos/:id", TodoController.update)
+app.get("/", TodoController.getTodo)
+app.get("/search", TodoController.getByName)
+app.post("/insert", TodoController.create)
+app.delete("/delete", TodoController.delete)
+app.put("/:id", TodoController.update)
 
 // start express server
 const PORT = process.env.PORT || 5002
